c48fee123c7d1503862726aee2f088d72818542a
MelissaRodriguezHernandez <mrodriguezhernandez@cifpfbmoll.eu>
2023-02-13 12:39:43 +0100

    Proyecto completo

---
+++ app/src/main/java/com/android/example/wordlistsql/WordListOpenHelper.java
@@ -0,0 +1,146 @@
+package com.android.example.wordlistsql;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class WordListOpenHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = WordListOpenHelper.class.getSimpleName();
+    private static final int DATABASE_VERSION = 1;
+    private static final String WORD_LIST_TABLE = "word_entries";
+    private static final String DATABASE_NAME = "wordlist";
+    private SQLiteDatabase writeDB;
+    private SQLiteDatabase readDB;
+
+    //Nombre de las columnas
+    public static final String KEY_ID = "_id";
+    public static final String KEY_WORD = "word";
+
+    //Arrays de strins de las columnas
+    private static final String[] COLUMNS = {KEY_ID, KEY_WORD};
+
+    //Query que creara la tabla
+    private static final String WORD_LIST_TABLE_CREATE =
+            "CREATE TABLE " + WORD_LIST_TABLE + " (" +
+                    KEY_ID + " INTEGER PRIMARY KEY, " +
+                    KEY_WORD + " TEXT );";
+
+    public WordListOpenHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d(TAG, "Construct WordListOpenHelper");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(WORD_LIST_TABLE_CREATE);
+        fillDatabaseWithData(sqLiteDatabase);
+    }
+
+    private void fillDatabaseWithData(SQLiteDatabase sqLiteDatabase) {
+        String[] words = {"Android", "Adapter", "ListView", "AsyncTask", "Android Studio",
+                "SQLiteDatabase", "SQLOpenHelper", "Data model", "ViewHolder",
+                "Android Performance", "OnClickListener"};
+
+        //Contenedor para los valores
+        ContentValues values = new ContentValues();
+
+        for (int i=0; i < words.length;i++) {
+            // Put column/value pairs into the container. put() overwrites existing values.
+            values.put(KEY_WORD, words[i]);
+            sqLiteDatabase.insert(WORD_LIST_TABLE, null, values);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldV, int newV) {
+        Log.w(WordListOpenHelper.class.getName(),
+                "Upgrading database from version " + oldV + " to "
+                        + newV  + ", which will destroy all old data");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WORD_LIST_TABLE);
+        onCreate(sqLiteDatabase);
+    }
+
+    @SuppressLint("Range")
+    public WordItem query(int position) {
+        String query = "SELECT  * FROM " + WORD_LIST_TABLE +
+                " ORDER BY " + KEY_WORD + " ASC " +
+                "LIMIT " + position + ",1";
+
+        Cursor cursor = null;
+        WordItem wordItem = new WordItem();
+
+        try {
+            if (readDB == null) {readDB = getReadableDatabase();}
+            cursor = readDB.rawQuery(query, null);
+            cursor.moveToFirst();
+            wordItem.setId(cursor.getInt(cursor.getColumnIndex(KEY_ID)));
+            wordItem.setWord(cursor.getString(cursor.getColumnIndex(KEY_WORD)));
+        } catch (Exception e) {
+            Log.d(TAG, "QUERY EXCEPTION! " + e.getMessage());
+        } finally {
+
+            cursor.close();
+            return wordItem;
+        }
+    }
+
+    public long count() {
+        if (readDB == null) {readDB = getReadableDatabase();}
+        return DatabaseUtils.queryNumEntries(readDB, WORD_LIST_TABLE);
+    }
+
+    public long insert(String word) {
+        long newId = 0;
+        ContentValues values = new ContentValues();
+        values.put(KEY_WORD, word);
+        try {
+            if (writeDB == null) {writeDB = getWritableDatabase();}
+            newId = writeDB.insert(WORD_LIST_TABLE, null, values);
+        } catch (Exception e) {
+            Log.d(TAG, "INSERT EXCEPTION! " + e.getMessage());
+        }
+        return newId;
+    }
+
+    public int update(int id, String word) {
+        int mNumberOfRowsUpdated = -1;
+        try {
+            if (writeDB == null) {writeDB = getWritableDatabase();}
+            ContentValues values = new ContentValues();
+            values.put(KEY_WORD, word);
+
+            mNumberOfRowsUpdated = writeDB.update(WORD_LIST_TABLE, //table to change
+                    values, // new values to insert
+                    KEY_ID + " = ?", // selection criteria for row (in this case, the _id column)
+                    new String[]{String.valueOf(id)}); //selection args; the actual value of the id
+
+        } catch (Exception e) {
+            Log.d (TAG, "UPDATE EXCEPTION! " + e.getMessage());
+        }
+        return mNumberOfRowsUpdated;
+    }
+
+    public int delete(int id) {
+        int deleted = 0;
+        try {
+            if (writeDB == null) {writeDB = getWritableDatabase();}
+            deleted = writeDB.delete(WORD_LIST_TABLE, //table name
+                    KEY_ID + " = ? ", new String[]{String.valueOf(id)});
+        } catch (Exception e) {
+            Log.d (TAG, "DELETE EXCEPTION! " + e.getMessage());        }
+        return deleted;
+    }
+
+
+
+}
